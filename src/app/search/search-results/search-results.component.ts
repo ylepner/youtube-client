c9820192fcf181839a8c032c63256e96780997d1
Yulia Lepner <ylepner@gmail.com>
2022-03-30 22:21:17 +0200

    feat: update search result component

--- src/app/search/search-results/search-results.component.ts
+++ src/app/search/search-results/search-results.component.ts
@@ -4,7 +4,7 @@ import { SearchResultItem } from '../search-item.model'
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
-  styleUrls: ['./search-results.component.scss']
+  styleUrls: ['./search-results.component.scss'],
 })
 export class SearchResultsComponent {
 
