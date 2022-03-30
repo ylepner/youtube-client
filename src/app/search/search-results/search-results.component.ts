a27aec8b0b69f14478dff4b7d2d274608044a4b9
Yulia Lepner <ylepner@gmail.com>
2022-03-30 16:58:03 +0200

    feat: add items as input to search results

--- src/app/search/search-results/search-results.component.ts
+++ src/app/search/search-results/search-results.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { SearchResultItem } from '../search-item.model'
 
 @Component({
   selector: 'app-search-results',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class SearchResultsComponent {
 
+  @Input()
+  items?: SearchResultItem[];
 }
