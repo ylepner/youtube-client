89ea5d60bdbeeee59fe6c902589aa9afcfcdddd8
Yulia Lepner <ylepner@gmail.com>
2022-03-30 22:21:58 +0200

    feat: update style

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { SearchResultList } from 'src/app/search/search-response.model';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  result?: SearchResultList;
 
+  constructor(httpClient: HttpClient) {
+    httpClient.get<SearchResultList>('assets/data/data.json').subscribe(r => {
+      this.result = r;
+    })
+  }
 }
