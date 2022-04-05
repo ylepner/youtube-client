f0e51a6bcd5585403f3b92de9b7ef5f5c2bc06d5
Yulia Lepner <ylepner@gmail.com>
2022-04-05 14:26:16 +0200

    feat: add login page markup

--- src/app/auth/pages/login-page/login-page.component.spec.ts
+++ src/app/auth/pages/login-page/login-page.component.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginPageComponent } from './login-page.component';
@@ -8,9 +9,9 @@ describe('LoginPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginPageComponent ]
+      declarations: [LoginPageComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
