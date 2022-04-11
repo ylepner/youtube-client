940328fd2a96fbccd2cad5b82055d4715653c856
Yulia Lepner <ylepner@gmail.com>
2022-04-11 15:06:35 +0200

    feat: implement prettier rules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
