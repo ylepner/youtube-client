1a4f5a99bc4583d44bce38141d7ecd9c0fa10f0c
Yulia Lepner <ylepner@gmail.com>
2022-04-18 22:46:45 +0200

    feat: import ReactiveFormsModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [],
-  imports: [CommonModule, FormsModule],
-  exports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  exports: [CommonModule, FormsModule, ReactiveFormsModule],
 })
 export class SharedModule { }
