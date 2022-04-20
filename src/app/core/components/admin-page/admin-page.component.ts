9782bc39c245f00dd20a82d56cc58ff94c457d2d
Yulia Lepner <ylepner@gmail.com>
2022-04-20 20:39:53 +0200

    feat: add date input validation

--- src/app/core/components/admin-page/admin-page.component.ts
+++ src/app/core/components/admin-page/admin-page.component.ts
@@ -1,16 +1,16 @@
+/* eslint-disable no-unused-vars */
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
+const REGEXP_URL = '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?';
 @Component({
   selector: 'app-admin-page',
   templateUrl: './admin-page.component.html',
-  styleUrls: ['./admin-page.component.scss']
+  styleUrls: ['./admin-page.component.scss'],
 })
 export class AdminPageComponent {
-  constructor(
-    private router: Router
-  ) { }
+  constructor(private router: Router) { }
 
   cardForm = new FormGroup({
     title: new FormControl('', [
@@ -18,18 +18,27 @@ export class AdminPageComponent {
       Validators.minLength(3),
       Validators.maxLength(20),
     ]),
-    description: new FormControl('', [
-      Validators.maxLength(255),
-    ]),
+    description: new FormControl('', [Validators.maxLength(255)]),
     img: new FormControl('', [
       Validators.required,
-      Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?')
+      Validators.pattern(REGEXP_URL),
     ]),
     linkVideo: new FormControl('', [
       Validators.required,
-      Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?')
+      Validators.pattern(REGEXP_URL),
     ]),
-  })
+    date: new FormControl('', [
+      Validators.required,
+      (control) => {
+        const dateForm = new Date(control.value);
+        const today = new Date();
+        if (today.getTime() - dateForm.getTime() >= 0) {
+          return null;
+        }
+        return { dateError: 'The date is invalid' };
+      },
+    ]),
+  });
 
   get form() {
     return this.cardForm.controls;
@@ -37,7 +46,7 @@ export class AdminPageComponent {
 
   onSubmit() {
     if (this.cardForm.valid) {
-      this.router.navigate(['home'])
+      this.router.navigate(['home']);
     }
   }
 }
