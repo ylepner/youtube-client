1fef4f4280d12a0793f72dac192e3df20de07b1e
Yulia Lepner <ylepner@gmail.com>
2022-04-20 17:18:00 +0200

    feat: add validation form and rules

--- src/app/core/components/admin-page/admin-page.component.ts
+++ src/app/core/components/admin-page/admin-page.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-page',
   templateUrl: './admin-page.component.html',
   styleUrls: ['./admin-page.component.scss']
 })
-export class AdminPageComponent implements OnInit {
+export class AdminPageComponent {
+  constructor(
+    private router: Router
+  ) { }
 
-  constructor() { }
+  cardForm = new FormGroup({
+    title: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+    ]),
+    description: new FormControl('', [
+      Validators.maxLength(255),
+    ]),
+    img: new FormControl('', [
+      Validators.required,
+      Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?')
+    ]),
+    linkVideo: new FormControl('', [
+      Validators.required,
+      Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?')
+    ]),
+  })
 
-  ngOnInit(): void {
+  get form() {
+    return this.cardForm.controls;
   }
 
+  onSubmit() {
+    if (this.cardForm.valid) {
+      this.router.navigate(['home'])
+    }
+  }
 }
