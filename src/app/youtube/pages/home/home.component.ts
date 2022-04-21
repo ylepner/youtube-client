d21ffec8232bbd4350c7abd9695442c8b7b51c93
Yulia Lepner <ylepner@gmail.com>
2022-04-22 00:17:53 +0200

    feat: update home component

--- src/app/youtube/pages/home/home.component.ts
+++ src/app/youtube/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 /* eslint-disable no-unused-vars */
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { YoutubeService } from 'src/app/core/services/youtube.service';
+import { loadVideos } from 'src/app/redux/actions/youtube.actions';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +10,7 @@ import { YoutubeService } from 'src/app/core/services/youtube.service';
 })
 export class HomeComponent {
   items$ = this.youtubeService.itemsResult$;
-  constructor(private youtubeService: YoutubeService) { }
+  constructor(private youtubeService: YoutubeService, private store: Store) {
+    this.store.dispatch(loadVideos());
+  }
 }
