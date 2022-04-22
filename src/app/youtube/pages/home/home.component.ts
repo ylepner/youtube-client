2ffcad2575664954f23c357f2084bb18cc856744
Yulia Lepner <ylepner@gmail.com>
2022-04-22 22:38:46 +0200

    feat: update home component

--- src/app/youtube/pages/home/home.component.ts
+++ src/app/youtube/pages/home/home.component.ts
@@ -3,14 +3,14 @@ import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { YoutubeService } from 'src/app/core/services/youtube.service';
 import { loadVideos } from 'src/app/redux/actions/youtube.actions';
-import { selectApiVideos } from 'src/app/redux/selectors/youtube.selectors';
+import { selectApiVideos, selectFilteredVideos } from 'src/app/redux/selectors/youtube.selectors';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
-  items$ = this.store.select(selectApiVideos);
+  items$ = this.store.select(selectFilteredVideos);
   constructor(private store: Store) {
     this.store.dispatch(loadVideos());
   }
